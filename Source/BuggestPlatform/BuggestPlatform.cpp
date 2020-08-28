8fd22cde48c960c2d51fb983c81176c3444bca6e
Sergey Firsov <fearthunder@yandex.ru>
2020-08-28 12:55:09 +0300

    Init commit

---
+++ Source/BuggestPlatform/BuggestPlatform.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BuggestPlatform.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BuggestPlatform, "BuggestPlatform" );
