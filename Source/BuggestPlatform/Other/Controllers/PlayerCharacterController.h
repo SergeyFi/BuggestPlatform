1ba68f8e2c052121f113cf4aba2c0eef80b9f7f3
Sergey Firsov <fearthunder@yandex.ru>
2020-08-28 13:27:33 +0300

    Add player character base controller

---
+++ Source/BuggestPlatform/Other/Controllers/PlayerCharacterController.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "PlayerCharacterController.generated.h"
+
+
+UCLASS()
+class BUGGESTPLATFORM_API APlayerCharacterController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
