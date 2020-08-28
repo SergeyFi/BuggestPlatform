cb064098ba08946ab0cf76d6818506c8d13f8304
Sergey Firsov <fearthunder@yandex.ru>
2020-08-28 17:25:33 +0300

    Add stamina component

---
+++ Source/BuggestPlatform/Components/Stamina/StaminaComponent.cpp
@@ -0,0 +1,139 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "StaminaComponent.h"
+#include "GameFramework/Character.h"
+
+// Sets default values for this component's properties
+UStaminaComponent::UStaminaComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+
+	StaminaMax = 100.0f;
+}
+
+// Called when the game starts
+void UStaminaComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	BindMovementComponent();
+}
+
+void UStaminaComponent::RemoveStamina(float StaminaDamage, bool Silent)
+{
+	Stamina -= StaminaDamage;
+
+	if (Stamina < 0.0f || FMath::IsNearlyZero(Stamina))
+	{
+		Stamina = 0.0f;
+
+		if (!Silent)
+		{
+			OnStaminaEnded.Broadcast(Stamina);
+		}
+	}
+
+	if (StaminaRegenNeed)
+	{
+		StartStaminaRegen();
+	}
+
+	if (!Silent)
+	{
+		OnStamindaDamaged.Broadcast(StaminaDamage);
+	}
+	
+}
+
+void UStaminaComponent::AddStamina(float StaminaHeal, bool Silent)
+{
+	Stamina += StaminaHeal;
+
+	if (Stamina > StaminaMax)
+	{
+		Stamina = StaminaMax;
+	}
+
+	if (Stamina == StaminaMax)
+	{
+		if (!Silent)
+		{
+			OnStaminaFull.Broadcast(Stamina);
+		}
+	}
+
+	if (!Silent)
+	{
+		OnStaminaAdd.Broadcast(StaminaHeal);
+	}
+}
+
+float UStaminaComponent::GetStamina()
+{
+	return Stamina;
+}
+
+void UStaminaComponent::SetMaxStamina(float MaxStamina)
+{
+	StaminaMax = MaxStamina;
+}
+
+void UStaminaComponent::BindMovementComponent()
+{
+	OwnerCharacter = Cast<ACharacter>(GetOwner());
+
+	if (OwnerCharacter)
+	{
+		OwnerCharacter->MovementModeChangedDelegate.AddDynamic(this, &UStaminaComponent::OnMovementUpdate);
+	}
+}
+
+void UStaminaComponent::OnMovementUpdate(ACharacter* Character, EMovementMode MoveMode, uint8 somedata)
+{
+	if (MoveMode == EMovementMode::MOVE_Walking)
+	{
+		StartStaminaWaste();
+	}
+	else
+	{
+		StopStaminaWaste();
+	}
+}
+
+void UStaminaComponent::StartStaminaWaste()
+{
+	GetWorld()->GetTimerManager().SetTimer(TimerStaminaWaste, this,
+		&UStaminaComponent::StaminaWaste,StaminaWasteRate, true);
+}
+
+void UStaminaComponent::StaminaWaste()
+{
+	RemoveStamina(StaminaWasteAmount * StaminaWasteRate, true);
+}
+
+void UStaminaComponent::StopStaminaWaste()
+{
+	GetWorld()->GetTimerManager().ClearTimer(TimerStaminaWaste);
+}
+
+void UStaminaComponent::StartStaminaRegen()
+{
+	GetWorld()->GetTimerManager().SetTimer(TimerStaminaRegen, this,
+    &UStaminaComponent::StaminaRegen, StaminaWasteRate, true);
+}
+
+void UStaminaComponent::StaminaRegen()
+{
+	AddStamina(StaminaRegenAmount * StaminaRegenRate, true);
+
+	if (Stamina >= StaminaMax)
+	{
+		StopStaminaRegen();
+	}
+}
+
+void UStaminaComponent::StopStaminaRegen()
+{
+	GetWorld()->GetTimerManager().ClearTimer(TimerStaminaRegen);
+}
---
+++ Source/BuggestPlatform/Components/Stamina/StaminaComponent.h
@@ -0,0 +1,85 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "StaminaComponent.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FStaminaDelegate, float, Value);
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BUGGESTPLATFORM_API UStaminaComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UStaminaComponent();
+	
+	void RemoveStamina(float StaminaDamage, bool Silend = false);
+
+	void AddStamina(float StaminaHeal, bool Silent = false);
+
+	float GetStamina();
+
+	void SetMaxStamina(float MaxStamina);
+
+	FStaminaDelegate OnStaminaEnded;
+
+	FStaminaDelegate OnStamindaDamaged;
+
+	FStaminaDelegate OnStaminaAdd;
+
+	FStaminaDelegate OnStaminaFull;
+	
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float Stamina;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float StaminaMax;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float StaminaWasteRate;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float StaminaWasteAmount;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	bool StaminaRegenNeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float StaminaRegenRate;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float StaminaRegenAmount;
+	
+private:
+
+	class ACharacter* OwnerCharacter;
+
+	void BindMovementComponent();
+
+	UFUNCTION()
+	void OnMovementUpdate(class ACharacter* Character, EMovementMode MoveMode, uint8 somedata);
+
+	void StartStaminaWaste();
+
+	void StaminaWaste();
+
+	void StopStaminaWaste();
+
+	void StartStaminaRegen();
+
+	void StaminaRegen();
+
+	void StopStaminaRegen();
+
+	FTimerHandle TimerStaminaWaste;
+
+	FTimerHandle TimerStaminaRegen;
+};
