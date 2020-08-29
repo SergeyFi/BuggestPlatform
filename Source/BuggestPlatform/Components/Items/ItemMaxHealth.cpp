28c55217c191577a22ed1614672d96be608e9bbe
Sergey Firsov <fearthunder@yandex.ru>
2020-08-29 14:55:33 +0300

    Add max health item increase

---
+++ Source/BuggestPlatform/Components/Items/ItemMaxHealth.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ItemMaxHealth.h"
+#include "BuggestPlatform/Components/Health/HealthComponent.h"
+
+void UItemMaxHealth::OnTargetDetected(AActor* OtherActor)
+{
+    if (OtherActor)
+    {
+        auto HealthComponent = OtherActor->FindComponentByClass<UHealthComponent>();
+
+        if (HealthComponent)
+        {
+            HealthComponent->SetMaxHealth(HealthComponent->GetMaxHealth() + MaxHealthAmount);
+        }
+    }
+}
---
+++ Source/BuggestPlatform/Components/Items/ItemMaxHealth.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/Items/BaseItemComponent.h"
+#include "ItemMaxHealth.generated.h"
+
+
+UCLASS()
+class BUGGESTPLATFORM_API UItemMaxHealth : public UBaseItemComponent
+{
+	GENERATED_BODY()
+
+private:
+
+	UPROPERTY(EditAnywhere)
+	float MaxHealthAmount;
+
+	virtual void OnTargetDetected(AActor* OtherActor) override;
+};
